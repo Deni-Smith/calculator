483e5b90dba5e30fc749661f2264285a4badf34e
Denis Kuznecov <vdv-den@mail.ru>
2022-04-20 12:42:22 +0300

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+/* calculator.js */
+export function sum(a, b) {
+    // your code
+    return a + b 
+  }
+  
+  export function subtract(a, b) {
+    // your code
+    return a - b 
+  }
+  
+  export function multiply(a, b) {
+    // your code
+    return a * b
+  }
+  
+  export function divide(a, b) {
+    // your code
+     return a / b 
+  }
